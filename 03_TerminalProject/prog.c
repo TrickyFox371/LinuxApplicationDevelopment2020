7728f555ed4c8dc3e95eb2813496d6bd04b09b12
TrickyFox371 <chernokni@ya.ru>
2020-10-02 03:38:27 +0300

    Minimal functionality
    Signed-off-by: TrickyFox371 <chernokni@ya.ru>

---
+++ 03_TerminalProject/prog.c
@@ -0,0 +1,43 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Error: Wrong number of arguments\n");
+        return 0;
+    }
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        printf("Error: Can not open file\n");
+        return 0;
+    }
+    initscr();
+    noecho();
+    char *ret = argv[1];
+    while (ret != NULL)
+    {
+        erase();
+        addstr("File: ");
+        addstr(argv[1]);
+        addstr("\n\n");
+        int i = 3;
+        char str[COLS];
+        while ((ret = fgets(str, COLS, f)) != NULL && i < LINES - 1)
+        {
+            addstr(str);
+            ++i;
+        }
+        if (ret != NULL)
+        {
+            addstr(str);
+        }
+        int inp;
+        while ((inp = getch()) != ' ') { }
+    }
+    endwin();
+    return 0;
+}
